0490c1135dccd83ed18f8353a7d7b2d3418a368d
memory <2306821735@qq.com>
2023-06-26 23:16:28 +0800

    完成发布帖子和查看帖子详情

---
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -0,0 +1,60 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController {
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add",method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title,String content){
+        User user = hostHolder.getUser();
+        if(user==null){
+            return CommunityUtil.getJSONString(403,"你还没有登录哦！");
+        }
+
+        DiscussPost post = new DiscussPost();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setUserId(user.getId());
+        post.setCreateTime(new Date());
+        discussPostService.addDiscussPost(post);
+
+        return CommunityUtil.getJSONString(0,"发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}" ,method = RequestMethod.GET)
+    @ResponseBody
+    public String findDiscussPostById(@PathVariable("discussPostId") int id){
+        DiscussPost post = discussPostService.findDiscussPostById(id);
+        User user = userService.findUserById(post.getUserId());
+        Map<String, Object> map = new HashMap<>();
+        map.put("post", post);
+        map.put("user", user);
+        return CommunityUtil.getJSONString(0,"查询成功！",map);
+    }
+}
